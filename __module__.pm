c830b877bd31ba9539f48671da968076e5157266
xarlos <xarlos@command1.net.xarlos.me>
2015-12-31 15:26:38 +0000

    Defined the folder first before the persistent mount

--- __module__.pm
+++ __module__.pm
@@ -2,14 +2,14 @@ package Rex::Gluster::Client;
 use Rex -base;
 use Rex::Ext::ParamLookup;
 
-# Usage: rex setup server=gluster.my.domain.com
+# Usage: rex setup server=gluster.my.domain.com [ volname => "gv0" ] [ mountpoint => "/mnt/share/" ]
 # Usage: rex remove
 
 desc 'Set up gluster agent';
 task 'setup', sub { 
 
 	my $server     = param_lookup "server";
-	my $mountpoint = param_lookup "mountpoint", "/mnt/gluster";
+	my $mountpoint = param_lookup "mountpoint", "/mnt/share";
 	my $volname    = param_lookup "volname", "gv0";
 
 	unless ($server) {
@@ -26,6 +26,11 @@ task 'setup', sub {
 			on_change => sub { say "package was installed/updated"; };
  	};
 
+	file "$mountpoint",
+		ensure => "directory",
+		owner  => "root",
+		group  => "root";
+	
 	# mount persistent with entry in /etc/fstab
    	mount "$server:$volname", "$mountpoint",
 		ensure    => "persistent",
@@ -35,11 +40,6 @@ task 'setup', sub {
 			say "gluster point added to fstab"; 
 		};
 
-	file "$mountpoint",
-		ensure => "directory",
-		owner  => "root",
-		group  => "root";
-	
 	mount "$mountpoint";
 };
 
@@ -48,7 +48,7 @@ desc 'Remove gluster client';
 task 'clean', sub {
 
 	my $server     = param_lookup "server";
-	my $mountpoint = param_lookup "mountpoint", "/mnt/gluster";
+	my $mountpoint = param_lookup "mountpoint", "/mnt/share";
 	my $volname    = param_lookup "volname", "gv0";
 
 	unless ($server) {
